7cfc440f1890a0cc889679b6f4509842d10d486e
calebripley <calebaripley@gmail.com>
2016-02-28 15:41:20 -0700

    Initial Commit. Sign in and Sign up functionality complete

--- Pictogram/AppDelegate.swift
+++ Pictogram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Pictogram"
+                configuration.clientKey = "kndke0394fkefn3"
+                configuration.server = "https://mighty-brook-84647.herokuapp.com/parse"
+            })
+        )
+        
         return true
     }
 
